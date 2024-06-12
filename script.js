1428e6c945f54078f4eab854b2be0b8518662f81
Nguyen Huynh Nhat Minh <66111145+wingfam@users.noreply.github.com>
2024-06-12 17:14:15 +0700

    - added change book read status button - added changeReadStatus function - added displayReadStatus

--- script.js
+++ script.js
@@ -9,31 +9,55 @@ properties. Then store it in an array.
 each book on the page (either in table or in their own "card").
 4 [x]. Create a "New Book" button that bring up a form allowing
 users to input the details of a new book.
-5 []. Add a button on each book's display to remove the book
+5 [x]. Add a button on each book's display to remove the book
 from the library (need to associate the DOM element with the
 actual book object. One solution is add a data-attribute
 that corresponds to the index of the library array).
-6 []. Add a button to change isRead status of each book.
+6 [X]. Add a button to change isRead status of each book.
 (create a function that toggles a book's isRead status on 
 the Book prototype instance)
 
 */
 
 const myLibrary = [];
-const tableHead = ["No.", "Title", "Author", "Pages", "Have you read it?", "Actions"];
 const newBookDialog = document.querySelector("#new-book-dialog");
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages, readStatus) {
   // constructor
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.isRead = isRead;
+  this.readStatus = readStatus;
+
+  this.toggleReadStatus = function () {
+    if (this.readStatus == "Not started") {
+      this.readStatus = "Reading";
+    } else if (this.readStatus == "Reading") {
+      this.readStatus = "Done";
+    } else if (this.readStatus == "Done") {
+      this.readStatus = "Not started";
+    }
+  };
 }
 
-function insertBookToMyLibrary(title, author, pages, isRead) {
-  const newBook = new Book(title, author, pages, isRead);
+function insertBookToMyLibrary(title, author, pages, readStatus) {
+  const newBook = new Book(title, author, pages, readStatus);
   myLibrary.push(newBook);
+}
+
+function removeBookAtIndex(element) {
+  const rowIndex = element.parentNode.rowIndex;
+  myLibrary.splice(rowIndex - 1, 1);
+  updateTableDisplay();
+}
+
+function changeReadStatus(book, element) {
+  book.toggleReadStatus();
+  const rowIndex = element.parentNode.rowIndex;
+  const statusColumn = document.querySelector(
+    "table tbody tr:nth-child(" + rowIndex + ") td:nth-child(5)"
+  );
+  displayReadStatus(book, statusColumn);
 }
 
 function displayLastBook() {
@@ -41,10 +65,9 @@ function displayLastBook() {
   insertNewRow(lastBook);
 }
 
-function removeBookAtIndex(element) {
-  rowIndex = element.parentNode.rowIndex;
-  myLibrary.splice(rowIndex - 1, 1);
-  updateTableDisplay();
+function showNewBookDialog() {
+  const newBookForm = document.querySelector("#add-new-book-form");
+  newBookForm.reset(); // clear form
 }
 
 function updateTableDisplay() {
@@ -59,6 +82,32 @@ function updateTableDisplay() {
   });
 }
 
+function displayReadStatus(book, statusColumn) {
+  if (book["readStatus"] == "Not started") {
+    statusColumn.textContent = "Not started";
+  } else if (book["readStatus"] == "2") {
+    statusColumn.textContent = "Reading";
+  } else if (book["readStatus"] == 3) {
+    statusColumn.textContent = "Done";
+  }
+}
+
+function submitForm() {
+  if (addNewBookFromInput()) {
+    displayLastBook();
+    newBookDialog.close();
+  }
+}
+
+function loadTemplate() {
+  if (myLibrary.length == 0) {
+    insertBookToMyLibrary("new book 1", "minh", "10", 1);
+    insertBookToMyLibrary("new book 2", "minh", "15", 2);
+    insertBookToMyLibrary("new book 3", "minh", "10", 3);
+    updateTableDisplay();
+  }
+}
+
 function insertNewRow(book) {
   const tbodyEle = document.querySelector("#book-table tbody");
   const trEle = document.createElement("tr");
@@ -71,6 +120,9 @@ function insertNewRow(book) {
   trEle.appendChild(thEle);
 
   for (const prop in book) {
+    // when the loop reach toggleReadStatus function,
+    // it won't add to the table and continue to next the prop
+    if (prop === "toggleReadStatus") continue;
     const tdEle = document.createElement("td");
     tdEle.textContent = book[prop].toString();
     trEle.appendChild(tdEle);
@@ -78,18 +130,19 @@ function insertNewRow(book) {
 
   const actionTdEle = document.createElement("td");
   const removeBtn = document.createElement("button");
+  const changeReadStatusBtn = document.createElement("button");
 
   removeBtn.textContent = "Remove";
+  changeReadStatusBtn.textContent = "Change Status";
   removeBtn.addEventListener("click", () => removeBookAtIndex(actionTdEle));
+  changeReadStatusBtn.addEventListener("click", () =>
+    changeReadStatus(book, actionTdEle)
+  );
 
   tbodyEle.appendChild(trEle);
   trEle.appendChild(actionTdEle);
   actionTdEle.appendChild(removeBtn);
-}
-
-function showNewBookDialog() {
-  const newBookForm = document.querySelector("#add-new-book-form");
-  newBookForm.reset(); // clear form
+  actionTdEle.appendChild(changeReadStatusBtn);
 }
 
 function addNewBookFromInput() {
@@ -121,16 +174,6 @@ function addNewBookFromInput() {
   return isAdded;
 }
 
-function loadTemplate() {
-  insertBookToMyLibrary("new book 1", "minh", "10", "No, I haven't");
-  insertBookToMyLibrary("new book 2", "minh", "15", "Yes, I have");
-  insertBookToMyLibrary("new book 3", "minh", "10", "No, I haven't");
-
-  myLibrary.forEach((book) => {
-    insertNewRow(book);
-  });
-}
-
 function clearForm() {
   const title = document.querySelector("#title");
   const author = document.querySelector("#author");
@@ -142,13 +185,6 @@ function clearForm() {
   pages.value = "";
 
   readStatus != null ? (readStatus.checked = false) : null;
-}
-
-function submitForm() {
-  if (addNewBookFromInput()) {
-    displayLastBook();
-    newBookDialog.close();
-  }
 }
 
 function setup() {
